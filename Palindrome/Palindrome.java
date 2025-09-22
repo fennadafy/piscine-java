c8e4d0b43871e8df10f173a5a9ba7fc204034248
fennadaf <fatimaezzahraennadafy@gmail.com>
2025-09-22 18:55:52 +0100

    palindrome

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -2,6 +2,9 @@
 public class Palindrome {
 
     public static boolean isPalindrome(String s) {
+        if (s == null) {
+            return false;
+        }
         String first_part = s.toLowerCase().substring(0, s.length() / 2);
         String last_part = (s.length() % 2 == 0) ? s.toLowerCase().substring(s.length() / 2, s.length()) : s.toLowerCase().substring(s.length() / 2 + 1, s.length());
         // System.out.println(first_part);
