70aef3d0703642b5a3fa8657c676a9b00e4c0970
fennadaf <fatimaezzahraennadafy@gmail.com>
2025-09-22 17:51:08 +0100

    cleanExtract

---
+++ CleanExtract/CleanExtract.java
@@ -0,0 +1,43 @@
+
+public class CleanExtract {
+
+    public static String extract(String s) {
+        String[] arr = s.split("\\|");
+        String result = "";
+        String tomp = "";
+        int arr_length = 0;
+        for (String d : arr) {
+            if (d.contains(".")) {
+
+                boolean is_valid_text = false;
+                String withou = d.trim();
+                for (int i = 0; i < withou.length(); i++) {
+                    if (withou.charAt(i) == '.') {
+                        if (arr_length == arr.length - 1 && is_valid_text) {
+                            tomp = tomp + '.';
+                        }
+                        is_valid_text = !is_valid_text;
+                        continue;
+                    }
+                    if (withou.charAt(i) == ' ' && tomp.isEmpty()) {
+                        continue;
+                    }
+
+                    if (is_valid_text) {
+                        tomp = tomp + withou.charAt(i);
+                    }
+                }
+            } else {
+                tomp = tomp + d.trim();
+            }
+
+            result = result + tomp;
+            if (!tomp.isEmpty() && tomp.charAt(tomp.length() - 1) != ' ') {
+                result = result + " ";
+            }
+            arr_length++;
+            tomp = "";
+        }
+        return result;
+    }
+}
