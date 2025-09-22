2904db70e2832fc40c9a0df16d2001c96230662a
fennadaf <fatimaezzahraennadafy@gmail.com>
2025-09-22 18:52:40 +0100

    palindrome

---
+++ Palindrome/Palindrome.java
@@ -0,0 +1,23 @@
+
+public class Palindrome {
+
+    public static boolean isPalindrome(String s) {
+        String first_part = s.substring(0, s.length() / 2);
+        String last_part = (s.length() % 2 == 0) ? s.substring(s.length() / 2, s.length()) : s.substring(s.length() / 2 + 1, s.length());
+        // System.out.println(first_part);
+        // System.out.println(reverce(last_part));
+        // if (first_part == reverce(last_part)){
+        //     return true
+        // }
+        // return false;
+        return first_part.equals(reverce(last_part));
+    }
+
+    public static String reverce(String s) {
+        String res = "";
+        for (int i = 0; i < s.length(); i++) {
+            res = s.charAt(i) + res;
+        }
+        return res;
+    }
+}
