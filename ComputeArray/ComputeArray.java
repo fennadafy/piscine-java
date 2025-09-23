f45e71068e2540aecd52bf8af7c945df8eae98fb
fennadaf <fatimaezzahraennadafy@gmail.com>
2025-09-23 10:22:29 +0100

    computeArrat

--- ComputeArray/ComputeArray.java
+++ ComputeArray/ComputeArray.java
@@ -1,23 +1,15 @@
+
 public class ComputeArray {
+
     public static int[] computeArray(int[] array) {
         int[] result = new int[array.length];
-        for (int i =0 ; i<array.length ; i++){
-            if (array[i] %3 ==0){
-                // System.out.println("r");
-                System.out.println(array[i]);
-                result[i] = array[i] *5;
-            }
-            if (array[i]%3 ==1){
-                // System.out.println("t");
-                System.out.println(array[i]);
-
-                result[i]= array[i] + 7 ;
-            }
-            if (array[i]%3 ==2){
-                // System.out.println("y");
-                System.out.println(array[i]);
-
-                result[i]=array[i];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 3 == 0) {
+                result[i] = array[i] * 5;
+            }else if (array[i] % 3 == 1) {
+                result[i] = array[i] + 7;
+            } else {
+                result[i] = array[i];
             }
         }
         return result;
