7d64743095ff455e4fad64050131cdea99d5130c
fennadaf <fatimaezzahraennadafy@gmail.com>
2025-09-23 16:30:53 +0100

    cat

--- Cat/Cat.java
+++ Cat/Cat.java
@@ -7,10 +7,17 @@ public class Cat {
     public static void cat(String[] args) throws IOException {
 
         for (String path : args) {
+           int bytesToRead = 100;
             try (FileInputStream fis = new FileInputStream(path)) {
+                byte[] buffer = new byte[bytesToRead];
+
                 int byteRead;
-                while ((byteRead = fis.read()) != -1) {
-                    System.out.write(byteRead);
+                // if (byteRead != -1){
+                    // String content = new String(buffer, 0, byteRead);
+
+                // }
+                while ((byteRead = fis.read(buffer , 0 , bytesToRead) ) != -1) {
+                    System.out.write(buffer,0, byteRead);
                 }
             }
         }
