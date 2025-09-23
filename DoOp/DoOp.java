c01260b1da1fe84d8d53c5144640057b2c7789c3
fennadaf <fatimaezzahraennadafy@gmail.com>
2025-09-23 11:58:54 +0100

    DoOp

--- DoOp/DoOp.java
+++ DoOp/DoOp.java
@@ -6,28 +6,29 @@ public class DoOp {
         String operation = "";
         int first = 0;
         int sec = 0;
-        if (args.length == 0 ){
-            return "Error" ;
+        if (args.length == 0) {
+            return "Error";
         }
         first = Integer.parseInt(args[0]);
-
-        operation = args[1];
+        if (args.length == 2) {
+            operation = args[1];
+        }
 
         sec = Integer.parseInt(args[2]);
 
         if (operation == "+") {
             return Integer.toString(first + sec);
         } else if (operation == "-") {
-           return  Integer.toString(first - sec);
+            return Integer.toString(first - sec);
         } else if (operation == "%") {
             if (sec == 0) {
                 return "Error";
             }
-            return  Integer.toString(first % sec);
-        }else if (operation == "*"){
-            return  Integer.toString(first * sec);
-        }else if (operation == "/"){
-             if (sec == 0) {
+            return Integer.toString(first % sec);
+        } else if (operation == "*") {
+            return Integer.toString(first * sec);
+        } else if (operation == "/") {
+            if (sec == 0) {
                 return "Error";
             }
             return Integer.toString(first / sec);
