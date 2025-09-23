90730af0b129b6bb009dc3672bac903f8ea0bbfa
fennadaf <fatimaezzahraennadafy@gmail.com>
2025-09-23 18:44:15 +0100

    catInFile

---
+++ CatInFile/CatInFile.java
@@ -0,0 +1,25 @@
+
+import java.io.*;
+
+public class CatInFile {
+
+    public static void cat(String[] args) throws IOException {
+        for (String path : args) {
+            InputStream br = System.in;
+            FileOutputStream fileOutputStream = null;
+            try {
+
+                fileOutputStream = new FileOutputStream(path);
+
+                byte[] buffer = new byte[100];
+                int byteRead;
+                while ((byteRead = br.read(buffer)) != -1) {
+                    fileOutputStream.write(buffer, 0, byteRead);
+
+                }
+            } catch (IOException ioe) {
+
+            }
+        }
+    }
+}
