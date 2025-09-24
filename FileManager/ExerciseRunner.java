a6f0bf42f71ffe4616611cd1ff01ebdf2e7adb6a
fennadaf <fatimaezzahraennadafy@gmail.com>
2025-09-24 12:45:41 +0100

    capitalize

--- FileManager/ExerciseRunner.java
+++ FileManager/ExerciseRunner.java
@@ -2,8 +2,12 @@ import java.io.IOException;
 
 public class ExerciseRunner {
     public static void main(String[] args) throws IOException {
-        FileManager.createFile("file.txt", "Lorem ipsum");
-        System.out.println(FileManager.getContentFile("file.txt"));
-        FileManager.deleteFile("file.txt");
+        FileManager.createFile("fileName.file","""
+                    This file has many lines
+                    Here is the second.
+                    And there is the 3rd !
+                    """ );
+        System.out.println(FileManager.getContentFile("fileName.file"));
+        // FileManager.deleteFile("file.txt");
     }
 }
--- FileManager/FileManager.java
+++ FileManager/FileManager.java
@@ -1,15 +1,15 @@
 
-import java.io.BufferedWriter;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileManager {
 
     public static void createFile(String fileName, String content) throws IOException {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+        try (FileWriter writer = new FileWriter(fileName)) {
             writer.write(content);
         } catch (IOException e) {
             System.out.println("An error occurred while writing to the file: " + e.getMessage());
@@ -17,19 +17,16 @@ public class FileManager {
     }
 
     public static String getContentFile(String fileName) throws IOException {
-        int bytesToRead = 100;
-        byte[] filecontent = null;
-        try (FileInputStream fis = new FileInputStream(fileName); ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
-            int byteRead;
-            byte[] buffer = new byte[bytesToRead];
-            while ((byteRead = fis.read(buffer , 0, bytesToRead)) != -1) { 
-                bos.write(buffer , 0, bytesToRead);
-            }
-            filecontent = bos.toByteArray();
+        String filecontent = null;
+        Path filepath = Paths.get(fileName);
+        try {
+            filecontent = Files.readString(filepath);
         } catch (IOException e) {
-             System.out.println("An error to reding from file: " + e.getMessage());
+            System.out.println("An error to reding from file: " + e.getMessage());
         }
-            return new String(filecontent);
+        // System.out.println(new String(filecontent));
+
+        return new String(filecontent);
     }
 
     public static void deleteFile(String fileName) {
