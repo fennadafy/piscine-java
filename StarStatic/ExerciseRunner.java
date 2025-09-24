9277d2c03c322d6928f683a57783e31c9451a5b2
fennadaf <fatimaezzahraennadafy@gmail.com>
2025-09-24 17:56:33 +0100

    start startic

---
+++ StarStatic/ExerciseRunner.java
@@ -0,0 +1,13 @@
+
+public class ExerciseRunner {
+
+    public static void main(String[] args) {
+        CelestialObject defaultStar = new CelestialObject();
+        CelestialObject earth = new CelestialObject("Terre", 1.0, 2.0, 2.0);
+
+        // defaultStar.getDistanceBetween(earth, earth);
+        System.out.println(CelestialObject.getDistanceBetween(defaultStar, earth));
+        System.out.println(CelestialObject.getDistanceBetweenInKm(defaultStar, earth));
+        // System.out.println(CelestialObject.KM_IN_ONE_AU);
+    }
+}
