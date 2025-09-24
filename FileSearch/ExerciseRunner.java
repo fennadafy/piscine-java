661ae85172b88134d01e9525cc8f2ae2b2d77310
fennadaf <fatimaezzahraennadafy@gmail.com>
2025-09-24 15:11:52 +0100

    fileserch

--- FileSearch/ExerciseRunner.java
+++ FileSearch/ExerciseRunner.java
@@ -2,6 +2,6 @@ import java.io.IOException;
 
 public class ExerciseRunner {
     public static void main(String[] args) throws IOException {
-        System.out.println(FileSearch.searchFile("file.txt"));
+        System.out.println(FileSearch.searchFile("file.xml"));
     }
 }
--- FileSearch/FileSearch.java
+++ FileSearch/FileSearch.java
@@ -4,18 +4,23 @@ import java.io.File;
 public class FileSearch {
 
     public static String searchFile(String fileName) {
-        String result = "documents/";
         File doc = new File("documents");
         File arr[] = doc.listFiles();
         return RecursivePrint(arr, 0, fileName);
     }
 
     static String RecursivePrint(File[] files, int level, String fileName) {
+        if (files == null) {
+            return null;
+        }
         for (File f : files) {
             if (f.isFile() && fileName.equals(f.getName())) {
-                return  f.getPath();
+                return f.getPath();
             } else if (f.isDirectory()) {
-               return RecursivePrint(f.listFiles(), level + 1, fileName);
+                String rec = RecursivePrint(f.listFiles(), level + 1, fileName);
+                if (rec != null) {
+                    return rec;
+                }
             }
         }
         return null;
