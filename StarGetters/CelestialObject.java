ccd7bb31db75c732036a723dbffc049b8b4b8548
fennadaf <fatimaezzahraennadafy@gmail.com>
2025-09-24 17:11:23 +0100

    stargetter

---
+++ StarGetters/CelestialObject.java
@@ -0,0 +1,54 @@
+
+public class CelestialObject {
+
+    public double x;
+    public double y;
+    public double z;
+    public String name;
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setX(double new_x) {
+        x = new_x;
+    }
+
+    public void setY(double new_y) {
+        y = new_y;
+    }
+
+    public void setZ(double new_z) {
+        z = new_z;
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public CelestialObject() {
+        x = 0.0;
+        y = 0.0;
+        z = 0.0;
+        name = "Soleil";
+    }
+
+    public CelestialObject(String name1, double x1, double y1, double z1) {
+        x = x1;
+        y = y1;
+        z = z1;
+        name = name1;
+    }
+}
