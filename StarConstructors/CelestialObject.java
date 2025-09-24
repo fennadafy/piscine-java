b9c9c3f732c054c9334bee428e487c1c36b64941
fennadaf <fatimaezzahraennadafy@gmail.com>
2025-09-24 16:53:32 +0100

    starProperties

--- StarConstructors/CelestialObject.java
+++ StarConstructors/CelestialObject.java
@@ -1,9 +1,21 @@
-public  class CelestialObject {
-   public double   x;
-   public double   y;
-   public double   z;
-   public String   name;
+public class CelestialObject {
 
-    
+    public double x;
+    public double y;
+    public double z;
+    public String name;
 
+    public CelestialObject() {
+        x = 0.0;
+        y = 0.0;
+        z = 0.0;
+        name = "Soleil";
+    }
+
+    CelestialObject(String name1, double x1, double y1, double z1) {
+        x = x1;
+        y = y1;
+        z = z1;
+        name = name1;
+    }
 }
