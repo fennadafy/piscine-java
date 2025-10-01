679dedd81825302e7cd83ed87022da3385b796bc
fennadaf <fatimaezzahraennadafy@gmail.com>
2025-10-01 14:33:37 +0100

    adventure

--- AdventureCharacter/Character.java
+++ AdventureCharacter/Character.java
@@ -1,6 +1,6 @@
 public class Character {
     private final int maxHealth;
-    public int currentHealth;
+    private int currentHealth;
     private final String name;
 
     public int getmaxHealth(){
@@ -14,7 +14,6 @@ public class Character {
     }
   
     public Character(String name , int maxHealth){
-        // System.out.println("llll");
         this.maxHealth = maxHealth;
         this.currentHealth = maxHealth;
         this.name = name;
