3b982f285681a388a7d981cfb83a1a8ad3e95698
fennadaf <fatimaezzahraennadafy@gmail.com>
2025-10-01 14:29:02 +0100

    adventure

---
+++ AdventureCharacter/Character.java
@@ -0,0 +1,39 @@
+public class Character {
+    private final int maxHealth;
+    public int currentHealth;
+    private final String name;
+
+    public int getmaxHealth(){
+        return this.maxHealth;
+    }
+    public int getcurrentHealth(){
+        return this.currentHealth;
+    }
+    public String getname(){
+       return this.name;
+    }
+  
+    public Character(String name , int maxHealth){
+        // System.out.println("llll");
+        this.maxHealth = maxHealth;
+        this.currentHealth = maxHealth;
+        this.name = name;
+    }
+    // @Override
+    public String toString(){
+        if (currentHealth <= 0 ){
+            // return String.format("% : KO", name);
+            return name + " : KO";
+        }
+        // return String.format("% : %/%", name, currentHealth, maxHealth);
+        return name + " : "+currentHealth + "/"+ maxHealth;
+    }
+
+    public void attack(Character character){
+       character.takeDamage(9);
+    }
+
+    public void takeDamage(int amount){
+        this.currentHealth = currentHealth - amount;
+    }
+}
