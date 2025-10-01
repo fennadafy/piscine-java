9f7164b4c53fe2d0b8fba5738a7caf46462e352e
fennadaf <fatimaezzahraennadafy@gmail.com>
2025-10-01 17:28:52 +0100

    monster

---
+++ AdventureMonster/Monster.java
@@ -0,0 +1,14 @@
+public class  Monster extends Character {
+ 
+
+    public Monster(String name , int maxHealth) {
+       super(name, maxHealth);
+    }
+    
+       public String toString() {
+        if (getCurrentHealth() <= 0 ){
+            return getName() + " is a monster with " + getCurrentHealth() + " HP";
+        }
+        return getName() + " is a monster with " + getCurrentHealth() + " HP";
+    }
+}
