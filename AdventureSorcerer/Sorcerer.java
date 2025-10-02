dcf7bd1d0e5558675dff29b0fee56cbe74a87657
fennadaf <fatimaezzahraennadafy@gmail.com>
2025-10-02 15:31:21 +0100

    sorcere

--- AdventureSorcerer/Sorcerer.java
+++ AdventureSorcerer/Sorcerer.java
@@ -12,9 +12,9 @@ public class Sorcerer extends Character implements Healer {
 
     public void heal(Character character){
         int current = character.getCurrentHealth();
-      
-
-        if (this.getHealCapacity() + current <= character.getMaxHealth()) {
+        if (this.getHealCapacity() + current == 20) {
+            character.setCurrentHealth(healCapacity + current);
+        } else if (this.getHealCapacity() + current < character.getMaxHealth()) {
             character.setCurrentHealth(healCapacity + current);
         } else {
             setCurrentHealth(character.getMaxHealth());
