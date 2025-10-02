20d8c91d08acc716f9a07434e288a3e288223914
fennadaf <fatimaezzahraennadafy@gmail.com>
2025-10-02 19:17:00 +0100

    abstract

---
+++ AdventureWeapon/Healer.java
@@ -0,0 +1,7 @@
+
+public interface Healer {
+
+    public void heal(Character character);
+
+    public int getHealCapacity();
+}
---
+++ AdventureWeapon/Templar.java
@@ -0,0 +1,58 @@
+
+public class Templar extends Character implements Healer, Tank {
+
+    private final int healCapacity;
+    private final int shield;
+
+    public Templar(String name, int maxHealth, int healCapacity, int shield, Weapon weapon) {
+        super(name, maxHealth,weapon);
+        this.shield = shield;
+        this.healCapacity = healCapacity;
+    }
+
+    public int getHealCapacity() {
+        return healCapacity;
+    }
+
+    public int getShield() {
+        return shield;
+    }
+    
+    public void heal(Character c) {
+        int current = c.getCurrentHealth() + healCapacity;
+
+        if (current > c.getMaxHealth()) {
+            c.setCurrentHealth(c.getMaxHealth());
+        } else {
+            c.setCurrentHealth(current);
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (getCurrentHealth() <= 0) {
+            return getName() + " has been beaten, even with its " + shield + " shield. So bad, it could heal " + healCapacity + " HP.";
+        }
+        return getName() + " is a strong Templar with " + getCurrentHealth() + " HP. It can heal " + healCapacity + " HP and has a shield of " + shield + ".";
+    }
+
+    @Override
+    public void attack(Character character) {
+       this.heal(this);
+        if (this.getWeapon() == null) {
+           character.takeDamage(6);
+        }else {
+            character.takeDamage(this.getWeapon().getDamage());
+        }
+    }
+
+    @Override
+    public void takeDamage(int damage) {
+        int sub_shield = (damage - shield);
+        if (super.getCurrentHealth() -  sub_shield<= 0) {
+            super.setCurrentHealth(0);
+        } else {
+            super.setCurrentHealth(super.getCurrentHealth() - sub_shield);
+        }
+    }
+}
