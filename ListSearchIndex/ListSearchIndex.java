69954d82924eb9ea3562c2a94d72771d31f4c9a0
fennadaf <fatimaezzahraennadafy@gmail.com>
2025-10-03 17:28:44 +0100

    listequals

---
+++ ListSearchIndex/ListSearchIndex.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class ListSearchIndex {
+    public static Integer findLastIndex(List<Integer> list, Integer value) {
+        // your code here
+    }
+    public static Integer findFirstIndex(List<Integer> list, Integer value) {
+        // your code here
+    }
+    public static List<Integer> findAllIndexes(List<Integer> list, Integer value) {
+        // your code here
+    }
+}
