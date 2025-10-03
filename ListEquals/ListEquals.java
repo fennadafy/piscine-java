bf7cbaf666e9d828ebaf553b34d758f02ab165f1
fennadaf <fatimaezzahraennadafy@gmail.com>
2025-10-03 16:59:58 +0100

    listequals

--- ListEquals/ListEquals.java
+++ ListEquals/ListEquals.java
@@ -2,7 +2,7 @@ import java.util.List;
 
 public class ListEquals {
     public static boolean areListsEqual(List<String> list1, List<String> list2) {
-        if (list1 ==null || list2 ==null){
+        if (list1 == null || list2 ==null || list1.isEmpty()){
             return false;
         }
         return list1.equals(list2);
