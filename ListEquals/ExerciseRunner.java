d521e1fda59b334e8f6f53b111b760ee9f1ade30
fennadaf <fatimaezzahraennadafy@gmail.com>
2025-10-03 17:10:08 +0100

    listequals

--- ListEquals/ExerciseRunner.java
+++ ListEquals/ExerciseRunner.java
@@ -2,7 +2,7 @@ import java.util.List;
 
 public class ExerciseRunner {
     public static void main(String[] args) {
-        System.out.println(ListEquals.areListsEqual(List.of("Alice", "Bob", "Charly", "Emily"), List.of("Alice", "Bob", "Charly", "Emily")));
-        System.out.println(ListEquals.areListsEqual(List.of("Alice", "Bob", "Charly", "Emily"), List.of("Alice", "Bob", "Emily", "Charly")));
+        System.out.println(ListEquals.areListsEqual(List.of(), List.of()));
+        System.out.println(ListEquals.areListsEqual(List.of(), List.of("Alice", "Bob", "Emily", "Charly")));
     }
 }
